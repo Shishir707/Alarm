89f14c0364bcc6dbf9a39c3a50e68cece39ea972
Shishir707 <mirshr8@gmail.com>
2025-04-14 17:44:50 +0600

    implement button function

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var currentTime=null;
+
+function setAlarm(){
+    var user=document.getElementById("alarmTime").value ;
+    if {user}{
+        var msg=`Alarm Set for ${user}`
+        document.getElementById("status").innerText =msg ;
+        user = true;
+    }
+}
