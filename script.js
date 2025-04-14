3a8aaacda97738d0ab6b2fbf908c78bef418b373
Shishir707 <mirshr8@gmail.com>
2025-04-14 17:58:02 +0600

    added waiting function

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-
-var currentTime=null;
-
 function setAlarm(){
     var user=document.getElementById("alarmTime").value ;
-    if {user}{
+    if (user){
         var msg=`Alarm Set for ${user}`
         document.getElementById("status").innerText =msg ;
-        user = true;
+        setAlarm= true;
     }
 }
+
+waitTime((){
+    if(setAlarm){
+        var now = new Date();
+        var nowTime = now.toTimeString().slice(0,5);
+        if (nowTime==user){
+            alert("⏰ Alarm! Time's up!");
+            setAlarm=false;
+            document.getElementById("status").innerText=""
+
+        }
+    }
+},1000);
